a6375da8a7582dc3b09e229f0a6e3de27b864ce0
Jupiter Zhuo <jupiterc@gmail.com>
2019-08-20 13:17:54 +0700

    no message

---
+++ day04/05.RestExample/src/main/java/id/equity/RestExample/configs/SecurityConstants.java
@@ -0,0 +1,9 @@
+package id.equity.RestExample.configs;
+
+public class SecurityConstants {
+	//SALT
+	public final static String SECRET = "";
+	public final static Long EXPIRED_TIME = 864000000L;// 10 Days
+	public final static String TOKEN_PREFIX ="Bearer ";
+	public final static String HEADER_STRING ="Authorization";
+}
