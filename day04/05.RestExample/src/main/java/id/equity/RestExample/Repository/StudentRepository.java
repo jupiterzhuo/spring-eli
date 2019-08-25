b49cab7b7d115e11f24af31a1bd6fc841b211314
Jupiter Zhuo <jupiterc@gmail.com>
2019-08-25 13:47:37 +0700

    Last update

--- day04/05.RestExample/src/main/java/id/equity/RestExample/Repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package id.equity.RestExample.Repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import id.equity.RestExample.models.Student;
-
-public interface StudentRepository extends CrudRepository<Student, Long> {
-
-}
--- day04/05.RestExample/src/main/resources/db/migration/V2__Insert_data.sql
+++
@@ -1,8 +0,0 @@
--- ----------------------------
--- Records of tbl_role
--- ----------------------------
-BEGIN;
-INSERT INTO `tbl_role` VALUES (1, 'Project Manager');
-INSERT INTO `tbl_role` VALUES (2, 'Programmer');
-INSERT INTO `tbl_role` VALUES (3, 'System Analyst');
-COMMIT;
